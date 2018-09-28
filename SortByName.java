c059251c9e64559d0e08b739d7b2e71b3a03debe
Kaitlin Bleich <kbleich@calpoly.edu>
2018-09-28 12:14:16 -0700

    done

---
+++ SortByName.java
@@ -0,0 +1,9 @@
+class Sortbyname implements Comparator<Student> 
+{ 
+       // Used for sorting in ascending order of 
+       //     // roll name 
+               public int compare(Student a, Student b) 
+               { 
+                  return a.name.compareTo(b.name); 
+               } 
+}
